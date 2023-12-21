a25f8ae45d299fdf500115bd8c2bafaab5567153
James Bawa <jbawa032@gmail.com>
2023-12-21 22:42:41 +0000

    rest parameter

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,10 @@
 export default function returnHowManyArguments(...restParameter) {
+/*
   let i = 0;
   restParameter.forEach(function(currentValue) {
     i++;
   });
   return i;
+*/
+  return restParameter.length;
 }
