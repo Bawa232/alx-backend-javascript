aac4e370e6ee9cd687a3675ff043a03f98edadbc
James Bawa <jbawa032@gmail.com>
2024-01-08 13:21:20 +0000

    returning objects of a class 3

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,7 @@
 import ClassRoom from './0-classroom.js'
 
-export default function initializeRooms {
-  return [19, 20, 34].map((num) => new ClassRoom(num));
+export default function initializeRooms() {
+  return [19, 20, 34].map((num) => {
+    new ClassRoom(num)
+  });
 }
