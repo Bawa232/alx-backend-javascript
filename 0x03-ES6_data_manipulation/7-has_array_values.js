f3e43ec161282b0da2085a05e466b74d655785c2
James Bawa <jbawa032@gmail.com>
2024-01-18 14:24:54 +0000

    all at once

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,9 @@
-export default function hasValuesFromArray(set, value) {
-  return value.map((val) => set.has(val));
+/**
+ * Checks if a set contains each element in an array.
+ * @param {Set} set - The collection of unique items.
+ * @param {*} array - The array of items.
+ * @returns {Boolean}
+ */
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
 }
